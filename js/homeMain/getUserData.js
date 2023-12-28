bc967e430b5470ccf5f6352983c72af2164f75f0
kawaii-poi <iria1314@qq.com>
2023-12-28 22:01:22 +0800

    modified:   js/homeMain/getUserData.js

--- js/homeMain/getUserData.js
+++ js/homeMain/getUserData.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: kawaii-poi iria1314@qq.com
+ * @Date: 2023-12-28 14:44:00
+ * @LastEditors: kawaii-poi iria1314@qq.com
+ * @LastEditTime: 2023-12-28 22:01:20
+ * @FilePath: /Miku/js/homeMain/getUserData.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 let userDataDiv = document.querySelector('.aboutme-main');
 
 let uName = userDataDiv.querySelector('.user-name');
