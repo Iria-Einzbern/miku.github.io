6bacda1ef43b2524858855559807da5c03fb8ed6
kawaii-poi <iria1314@qq.com>
2024-01-11 21:02:43 +0800

    modified:   css/main.css 	modified:   index.html 	modified:   js/homeMain/lazyLoad.js

--- js/homeMain/lazyLoad.js
+++ js/homeMain/lazyLoad.js
@@ -2,13 +2,24 @@
  * @Author: kawaii-poi iria1314@qq.com
  * @Date: 2023-12-27 15:53:13
  * @LastEditors: kawaii-poi iria1314@qq.com
- * @LastEditTime: 2024-01-11 19:42:41
+ * @LastEditTime: 2024-01-11 20:57:30
  * @FilePath: /Miku/js/homeMain/lazyLoad.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+
+let load
+
+//检测是否滚动到底部
+function isScrollAtBottom() {
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    const totalHeight = document.documentElement.scrollHeight;
+    const windowHeight = window.innerHeight;
+    
+    // 判断是否滚动到底部，可以根据需要调整阈值
+    return totalHeight - scrollTop <= windowHeight;
+}
 //封装为函数，在home-main中生成num个子div
 function createHSubDiv(num){
-    
     //获取父元素
     let homeMain = document.querySelector(".home-main")
     //在父元素中生成n个包含类名home-subDiv的子div元素
@@ -24,10 +35,21 @@ function createHSubDiv(num){
         homeMain.appendChild(homeSubDiv);
     }
 }
+
+
+
 function getVideoData(){
+    let loading = document.querySelector('.loading')
+    loading.classList.remove('noBlock')
 
+    clearTimeout(load)
+    //问题：每次都会加载两次，创建两组新的div
+    load = setTimeout(()=>{
+        loading.classList.add('noBlock')
+        createHSubDiv(9);
+    },700)
     
-
+    
     //获取所有包含empty类的视频元素块
     let homeMain = document.querySelector(".home-main")
     allEmpty = homeMain.querySelectorAll('.empty')
@@ -38,15 +60,10 @@ function getVideoData(){
 
 //检测滑动到底部后加载新的元素
 window.addEventListener('scroll', function() {
-    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-    var scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
-   
-    if (scrollTop + windowHeight == scrollHeight) {
-        //请求服务器加载元素
+    if (isScrollAtBottom()) {
         getVideoData();
     }
-  });
+});
 
 createHSubDiv(9);
 
